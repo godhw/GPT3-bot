56d693ebd4db38f98756cdf9b789d411e4e5c39e
godhw <shw0210@naver.com>
2022-07-13 18:26:39 +0900

    change code by eslint: airbnb style

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
-const fs = require("fs");
-const { Client, Collection, Intents } = require("discord.js");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
-const { token, client_id, test_guild_id } = process.env;
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const { token, clientId, testGuildId } = process.env;
 
 /**
  * From v13, specifying the intents is compulsory.
@@ -12,90 +13,73 @@ const { token, client_id, test_guild_id } = process.env;
  * @description Main Application Client */
 
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-/**********************************************************************/
 // Define Collection of Commands, Slash Commands and cooldowns
 client.slashCommands = new Collection();
 client.triggers = new Collection();
 client.cooldowns = new Collection();
 
-
-/**********************************************************************/
 // Below we will be making an event handler!
 fs
-	.readdirSync("./events")
-	.filter((file) => file.endsWith(".js"))
-	.map((file => {
-		const event = require(`./events/${file}`);
-		if (event.once) {
-			client.once(event.name, (...args) => event.execute(...args, client));
-		} else {
-			client.on(
-				event.name,
-				async (...args) => await event.execute(...args, client)
-			);
-		}
-	}))
+  .readdirSync('./events')
+  .filter((file) => file.endsWith('.js'))
+  .map(((file) => {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+      client.on(event.name, async (...args) => event.execute(...args, client));
+    }
+  }));
 
-
-/**********************************************************************/
 // Registration of Slash-Command Interactions.
 fs
-	.readdirSync(`./interactions/slash`)
-	.filter((file) => file.endsWith(".js"))
-	.map(commandFile => {
-		const command = require(`./interactions/slash/${commandFile}`);
-		client.slashCommands.set(command.data.name, command);
-	});
+  .readdirSync('./interactions/slash')
+  .filter((file) => file.endsWith('.js'))
+  .map((file) => {
+    const command = require(`./interactions/slash/${file}`);
+    client.slashCommands.set(command.data.name, command);
+  });
 
-
-
-/**********************************************************************/
 // Registration of Slash-Commands in Discord API
 
-const rest = new REST({ version: "9" }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(token);
 const commandJsonData = [...Array.from(client.slashCommands.values()).map((c) => c.data.toJSON())];
 
 (async () => {
-	try {
-		console.log("Started refreshing application (/) commands.");
+  try {
+    console.log('Started refreshing application (/) commands.');
 
-		await rest.put(
-			/**
-			 * Here we are sending to discord our slash commands to be registered.
-					There are 2 types of commands, guild commands and global commands.
-					Guild commands are for specific guilds and global ones are for all.
-					In development, you should use guild commands as guild commands update
-					instantly, whereas global commands take upto 1 hour to be published. To
-					deploy commands globally, replace the line below with:
-				Routes.applicationCommands(client_id)
-			 */
+    await rest.put(
+      /**
+       *  Here we are sending to discord our slash commands to be registered.
+          There are 2 types of commands, guild commands and global commands.
+          Guild commands are for specific guilds and global ones are for all.
+          In development, you should use guild commands as guild commands update
+          instantly, whereas global commands take upto 1 hour to be published. To
+          deploy commands globally, replace the line below with:
+        Routes.applicationCommands(client_id)
+       */
+      // Routes.applicationCommands(cliendId),
+      Routes.applicationGuildCommands(clientId, testGuildId),
+      { body: commandJsonData },
+    );
 
-			Routes.applicationCommands(client_id),
-			{ body: commandJsonData }
-
-			//Routes.applicationGuildCommands(client_id, test_guild_id),
-			//{ body: commandJsonData }
-		);
-
-		console.log("Successfully reloaded application (/) commands.");
-	} catch (error) {
-		console.error(error);
-	}
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
 })();
 
-
-/**********************************************************************/
 // Registration of Message Based Chat Triggers
-fs.readdirSync(`./triggers`)
-	.filter((file) => file.endsWith(".js"))
-	.map(file => {
-		const trigger = require(`./triggers/${file}`);
-		client.triggers.set(trigger.name, trigger);
-	})
-
+fs.readdirSync('./triggers')
+  .filter((file) => file.endsWith('.js'))
+  .map((file) => {
+    const trigger = require(`./triggers/${file}`);
+    client.triggers.set(trigger.name, trigger);
+  });
 
 // Login into your client application with bot's token.
 client.login(token);
--- triggers/chatbot.js
+++ triggers/chatbot.js
@@ -1,36 +1,42 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-const { api_endpoint } = process.env;
-//const { max_length } = process.env;
+const { apiEndpoint } = process.env;
+
 module.exports = {
-	keywords: ["codegpt"],
-	prefixes: ["code"],
-	suffixes: ["//"],
+  keywords: ['codegpt'],
+  prefixes: ['code'],
+  suffixes: ['//'],
 
-	/**
-	 * @description Executes when it is triggered by trigger handler.
-	 * @param {Object} message The Message Object of the trigger.
-	 * @param {String[]} args The Message Content of the received message seperated by spaces (' ') in an array
-	 */
+  /**
+   * @description Executes when it is triggered by trigger handler.
+   * @param {Object} message The Message Object of the trigger.
+   * @param {String[]} args The Message Content of the received message
+   * seperated by spaces (' ') in an array
+   */
 
-// edit code
-	async execute(message, args) {
-		console.log('Request: ', message.toString());
-    const requestMsg = message.toString().replace("//",'').replace("code",'');
+  // edit code
+  async execute(message, args) {
+    console.log('Request: ', message.toString());
+    const requestMsg = message.toString().replace('//', '').replace('code', '');
 
-    data = {'code': requestMsg,
-            'max_length': process.env.max_length}
+    const data = {
+      code: requestMsg,
+      max_length: process.env.max_length,
+    };
 
-		const responseBody = await fetch(api_endpoint, {method: 'POST', 
-      headers: { 'Content-Type': 'application/json',}, body: JSON.stringify(data),});
-		
+    const responseBody = await fetch(apiEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+
     const responseJson = await responseBody.json();
-    
-    const responseText = responseJson['text'];
 
-		console.log('Response: ', responseText, typeof(responseText));
-		message.reply({
-			content: responseText,
-		});
-	},
+    const responseText = responseJson.text;
+
+    console.log('Response: ', responseText, typeof responseText);
+    message.reply({
+      content: responseText,
+    });
+  },
 };
